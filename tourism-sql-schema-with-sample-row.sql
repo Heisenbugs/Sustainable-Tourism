42e0cc6af9defb3861abaa6b069e1f500c58956d
Sohan Chowdhury <sifat3d@gmail.com>
2016-04-02 14:12:56 +0600

    SQL

---
+++ tourism-sql-schema-with-sample-row.sql
@@ -0,0 +1,418 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.13.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 02, 2016 at 02:11 PM
+-- Server version: 5.6.28-0ubuntu0.15.10.1
+-- PHP Version: 5.6.11-1ubuntu3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tourism`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `address_id` int(11) NOT NULL,
+  `place_id` int(11) DEFAULT NULL,
+  `district_id` int(11) DEFAULT NULL,
+  `division_id` int(11) DEFAULT NULL,
+  `city_id` int(11) DEFAULT NULL,
+  `thana_id` int(11) DEFAULT NULL,
+  `country_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `address`
+--
+
+INSERT INTO `address` (`address_id`, `place_id`, `district_id`, `division_id`, `city_id`, `thana_id`, `country_id`) VALUES
+(1, 1, 2, 3, 0, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE IF NOT EXISTS `city` (
+  `city_id` int(11) NOT NULL,
+  `city_name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `city_des` varchar(512) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`city_id`, `city_name`, `city_des`) VALUES
+(0, 'Unavailable', 'N/A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE IF NOT EXISTS `country` (
+  `country_id` int(11) NOT NULL,
+  `country_code` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `country_name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`country_id`, `country_code`, `country_name`) VALUES
+(1, 'BD', 'Bangladesh');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `district`
+--
+
+CREATE TABLE IF NOT EXISTS `district` (
+  `district_id` int(11) NOT NULL,
+  `district_name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `district_description` varchar(512) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `district`
+--
+
+INSERT INTO `district` (`district_id`, `district_name`, `district_description`) VALUES
+(0, 'Unavailable', 'N/A'),
+(1, 'Chittagong', 'CTG'),
+(2, 'Sreemangal', 'SMGL'),
+(3, 'Bandarban', 'BBN'),
+(4, 'Khagrachari', 'KCI'),
+(5, 'Coxs Bazaar', 'CBZ'),
+(6, 'Comilla', 'CML'),
+(7, 'CTG Hill Districts', 'CTGHDS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `division`
+--
+
+CREATE TABLE IF NOT EXISTS `division` (
+  `division_id` int(11) NOT NULL,
+  `division_name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `division_description` varchar(512) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `division`
+--
+
+INSERT INTO `division` (`division_id`, `division_name`, `division_description`) VALUES
+(1, 'Chittagong', 'CTG'),
+(2, 'Dhaka', 'DHK'),
+(3, 'Sylhet', 'SYL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guides`
+--
+
+CREATE TABLE IF NOT EXISTS `guides` (
+  `guidesID` int(11) NOT NULL,
+  `guidesName` varchar(256) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organization`
+--
+
+CREATE TABLE IF NOT EXISTS `organization` (
+  `organizationID` int(11) NOT NULL,
+  `organizationName` varchar(512) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `places`
+--
+
+CREATE TABLE IF NOT EXISTS `places` (
+  `place_id` int(11) NOT NULL,
+  `place_type` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `place_name` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `place_description` varchar(2048) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `place_address` int(11) DEFAULT NULL,
+  `place_longitude` int(11) DEFAULT NULL,
+  `place_latitude` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `places`
+--
+
+INSERT INTO `places` (`place_id`, `place_type`, `place_name`, `place_description`, `place_address`, `place_longitude`, `place_latitude`) VALUES
+(1, 'Waterfall', 'Hummum Falls', 'This mystical water fall lies deep into the Rajkandi Reserve Forest. The water way (jhiri poth) of Hummum Falls is fascinating. The hills and the surrounding forest make this place an ideal camping site for the adventure seekers.', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `placetag`
+--
+
+CREATE TABLE IF NOT EXISTS `placetag` (
+  `placetag_primarykey` int(11) NOT NULL,
+  `place_id` int(11) NOT NULL DEFAULT '0',
+  `tag_id` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `placetag`
+--
+
+INSERT INTO `placetag` (`placetag_primarykey`, `place_id`, `tag_id`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `placetype`
+--
+
+CREATE TABLE IF NOT EXISTS `placetype` (
+  `name` varchar(100) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `Description` varchar(2048) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `placetype`
+--
+
+INSERT INTO `placetype` (`name`, `Description`) VALUES
+('Misc', 'N/A'),
+('Waterfall', 'Waterfall...');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `tag_id` int(11) NOT NULL,
+  `tag_name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tag_description` varchar(512) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`tag_id`, `tag_name`, `tag_description`) VALUES
+(1, 'Adventure', 'Example tag of adventure.'),
+(2, 'Family', 'Another Example');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thana`
+--
+
+CREATE TABLE IF NOT EXISTS `thana` (
+  `thana_id` int(11) NOT NULL,
+  `thana_name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `thana_code` varchar(16) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `thana`
+--
+
+INSERT INTO `thana` (`thana_id`, `thana_name`, `thana_code`) VALUES
+(0, 'Unavailable', 'N/A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `travellers`
+--
+
+CREATE TABLE IF NOT EXISTS `travellers` (
+  `traveller_id` int(11) NOT NULL,
+  `traveller_name` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `traveller_birthday` date NOT NULL,
+  `traveller_email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `traveller_password` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `traveller_active` smallint(6) NOT NULL,
+  `travellerCountry_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`address_id`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`city_id`);
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`country_id`);
+
+--
+-- Indexes for table `district`
+--
+ALTER TABLE `district`
+  ADD PRIMARY KEY (`district_id`);
+
+--
+-- Indexes for table `division`
+--
+ALTER TABLE `division`
+  ADD PRIMARY KEY (`division_id`);
+
+--
+-- Indexes for table `guides`
+--
+ALTER TABLE `guides`
+  ADD PRIMARY KEY (`guidesID`);
+
+--
+-- Indexes for table `organization`
+--
+ALTER TABLE `organization`
+  ADD PRIMARY KEY (`organizationID`);
+
+--
+-- Indexes for table `places`
+--
+ALTER TABLE `places`
+  ADD PRIMARY KEY (`place_id`);
+
+--
+-- Indexes for table `placetag`
+--
+ALTER TABLE `placetag`
+  ADD PRIMARY KEY (`placetag_primarykey`);
+
+--
+-- Indexes for table `placetype`
+--
+ALTER TABLE `placetype`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`tag_id`);
+
+--
+-- Indexes for table `thana`
+--
+ALTER TABLE `thana`
+  ADD PRIMARY KEY (`thana_id`);
+
+--
+-- Indexes for table `travellers`
+--
+ALTER TABLE `travellers`
+  ADD PRIMARY KEY (`traveller_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `address_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `country`
+--
+ALTER TABLE `country`
+  MODIFY `country_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `district`
+--
+ALTER TABLE `district`
+  MODIFY `district_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `division`
+--
+ALTER TABLE `division`
+  MODIFY `division_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `guides`
+--
+ALTER TABLE `guides`
+  MODIFY `guidesID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `organization`
+--
+ALTER TABLE `organization`
+  MODIFY `organizationID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `places`
+--
+ALTER TABLE `places`
+  MODIFY `place_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `placetag`
+--
+ALTER TABLE `placetag`
+  MODIFY `placetag_primarykey` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `thana`
+--
+ALTER TABLE `thana`
+  MODIFY `thana_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `travellers`
+--
+ALTER TABLE `travellers`
+  MODIFY `traveller_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
